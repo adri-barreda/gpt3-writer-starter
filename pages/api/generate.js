b559d2d94e0aa9bed7d9435ccf4758af93d3803c
adri-barreda <114175928+adri-barreda@users.noreply.github.com>
2023-01-08 18:41:50 +0000

    modified:   assets/buildspace-logo.png 	modified:   package-lock.json     modified:   package.json 	new file:   pages/api/generate.js 	modified:   pages/index.js 	new file:   yarn.lock

---
+++ pages/api/generate.js
@@ -0,0 +1,60 @@
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+const basePromptPrefix = 
+`
+Escríbeme una tabla de contenidos para un taller para adolescentes con el título de abajo.
+
+Título:`
+
+;
+const generateAction = async (req, res) => {
+  // Run first prompt
+  console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
+
+  const baseCompletion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `${basePromptPrefix}${req.body.userInput}`,
+    temperature: 0.8,
+    max_tokens: 2500,
+  });
+  
+  const basePromptOutput = baseCompletion.data.choices.pop();
+
+  // I build Prompt #2.
+  const secondPrompt = 
+  `
+  Eres un psicólogo muy reconocido. Extremadamente amable y encantado de proporcionar ayuda a tus pacientes.
+
+  Coge la tabla de contenidos y el título de abajo y genera un taller escrito para trabajar con adolescentes. No pongas solamente los puntos, explícalos como una historia. Profundiza en cada uno de ellos.
+
+  Título: ${req.body.userInput}
+
+
+  Tabla de contenidos:${basePromptOutput.text}
+
+  Taller:
+  `
+  
+  // I call the OpenAI API a second time with Prompt #2
+  const secondPromptCompletion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `${secondPrompt}`,
+    // I set a higher temperature for this one. Up to you!
+    temperature: 0.85,
+		// I also increase max_tokens.
+    max_tokens: 1250,
+  });
+  
+  // Get the output
+  const secondPromptOutput = secondPromptCompletion.data.choices.pop();
+
+  // Send over the Prompt #2's output to our UI instead of Prompt #1's.
+  res.status(200).json({ output: secondPromptOutput });
+};
+
+export default generateAction;
